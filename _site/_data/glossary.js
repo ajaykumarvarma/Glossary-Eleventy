ccbd035c307b6a7d48d803ff60f6e618dbc36925
N.AJAY KUMAR VARMA <90514443+ajaykumarvarma@users.noreply.github.com>
2023-09-30 17:54:44 +0530

    Add files via upload

---
+++ _site/_data/glossary.js
@@ -0,0 +1,12 @@
+module.exports = [
+    { term: "HTML", definition: "A standard markup language for documents designed to be displayed in a web browser." },
+    { term: "CSS", definition: "A stylesheet language used for describing the look and formatting of a document written in HTML." },
+    { term: "JavaScript", definition: "A programming language that enables interactive web features." },
+    { term: "API", definition: "Application Programming Interface, a set of rules that allows different software entities to communicate with each other." },
+    { term: "JSON", definition: "JavaScript Object Notation, a syntax for storing and exchanging data." },
+    { term: "XML", definition: "eXtensible Markup Language, a markup language that defines rules for encoding documents in a format that is both human-readable and machine-readable." },
+    { term: "HTTP", definition: "HyperText Transfer Protocol, the foundation of any data exchange on the Web and a protocol used for transmitting data." },
+    { term: "URL", definition: "Uniform Resource Locator, a reference or address for a resource on the Internet." },
+    { term: "UI", definition: "User Interface, the space where interactions between humans and machines occur." },
+    { term: "UX", definition: "User Experience, a person's emotions and attitudes about using a particular product, system or service." }
+  ];
